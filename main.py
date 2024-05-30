2ba0de56c8b240895bed047cf9101167fbb81038
Rabedon1 <rabedon1@espe.edu.ec>
2024-05-30 16:32:33 -0500

    Add the name in the Text

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import os
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    my_set = {"Hello World from Railway whit Flask!"}
+    my_set = {"Hello World my name is: Diego Saavedra"}
     return jsonify(list(my_set))
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
